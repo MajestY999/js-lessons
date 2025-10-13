d5ff00f5b851f548a900fd655bed209d4209303b
MajestY999 <Vilisovartem@gmail.com>
2025-10-13 22:00:15 +1000

    Switch

--- app.js
+++ app.js
@@ -1,14 +1,51 @@
-// Операторы равенства
-const secretNumber = 7; //но при использовании строкового типа, у нас будет отрабатываться блок,  только с не строгим  сравнением
-//Но чтобы строковый тип работал в строгом сравнение нужно его сделать число через Number(secretNumber)
-if (secretNumber === 7) {
-  console.log("Угадал  строго");
-}
-if (secretNumber == 7) {
-  console.log("Угадал не строго");
-}
+//Switch
+const role = "manager";
 
-const q = Number(prompt("Введите число")); //Здесь нужно приводить к виду числа так как prompt преобразовывает числа в строки  значения написанные в prompt
-if (q === 7) {
-  console.log("!");
+if (role === "manager") {
+  console.log("Менеджер");
+} else if (role === "admin") {
+  console.log("Админ");
+} else if (role === "ceo") {
+  console.log("CEO");
+} else {
+  console.log("Мы тебя не знаем");}
+switch (role) {
+  case "manager": // role === 'manager'
+    console.log("Менеджер");
+    break;
+  case "admin":
+    console.log("Админ");
+    break;
+  case "ceo":
+    console.log("CEO");
+    break;
+  default:
+    console.log(
+      "Мы тебя не знаем"
+    ); /* Switch лучше использовать только тогда когда сного вариантов ответа(от 3 и более),
+     а если их 2 или 1 то лучше использовать базово if else */}
+
+switch (role) {  // role === 'manager' 
+  case "manager":
+
+  case "admin":
+    console.log("Не руководитель");
+    break;
+  case "ceo":
+    console.log("Руководитель");
+    break;
+  default:
+    console.log("Мы тебя не знаем");}
+
+    const num = 1;
+
+switch(true){
+ case num > 0:  // true === num > 0
+    console.log('Положительный');
+    break;
+    case num < 0:
+        console.log('Отрицательный');
+        break;
+        default:
+            console.log('Ноль!');
 }
