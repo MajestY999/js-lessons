def37f580ab59fedf147cc49c8f3e3715d526b37
MajestY999 <Vilisovartem@gmail.com>
2025-10-15 15:48:28 +1000

    Логические операторы

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-// Булева логика 
+// Логические операторы 
+const isAdmin = true; // парава изменения файла
+const  canWrite = true; // право писать 
+const isUser = true;
 
+console.log(`Системный файл ${isAdmin && canWrite}`); // и 
+console.log(`Обычный файл ${isAdmin || canWrite}`);  //или
+console.log(`Инвертируем права админа ${!isAdmin}`);
 
+const isEdited = true;
+const isSuperAdmin = true;
 
+console.log(`Системный файл  с редактированием ${
+    isAdmin && canWrite && (!isEdited || isSuperAdmin)/* стал false */
+}`);
 
+let a = 7;
+if(a == -8 || a === 22) {
+    console.log();
+}
