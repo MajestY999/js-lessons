d17c23797244f23fb9d90ff5c274ba4efd7fc63b
MajestY999 <Vilisovartem@gmail.com>
2025-10-15 15:32:29 +1000

    Булева логика

--- app.js
+++ app.js
@@ -1,39 +1,5 @@
-// Упражнение - Проверка робота
+// Булева логика 
 
- const quest = Number(prompt("Сколько будет 7  + 15 или сколько будет 7 - 15"));
-
-if ((quest === 22, quest === -8)) {
-  console.log(alert(`Я не робот. Ответ: ${quest}`));
-} else {
-  console.log(alert(`Вы робот, так как ответ не верный.`));
-} 
-
-// Пример препоавателя
-
-const res = Number(prompt("Сколько будет 7  + 15 или  - 15?"));
-switch (true) {
-  case res === "Я не робот":
-  case Number(res) === 22:
-  case Number(res) === -8:
-    console.log("Успех");
-    break;
-  default:
-    console.log("Вы робот!");
-}
-
-if (res === "Я не робот") {
-  console.log("Успех");
-} else {
-  const resNum = Number(res);
-  switch (resNum) {
-    case 22:
-    case -8:
-      console.log(Успех);
-      break;
-    default:
-      console.log("Вы робот!");
-  }
-}
 
 
 
