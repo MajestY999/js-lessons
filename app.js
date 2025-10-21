bf45fc44435249897f02479df379a5cf4c4862e0
MajestY999 <Vilisovartem@gmail.com>
2025-10-21 22:02:46 +1000

    Упражнение -- Кредит на макбук

--- app.js
+++ app.js
@@ -1,22 +1,51 @@
-// Функции в функциях.
+// Упражнение -- Кредит на макбук
 
-const KG_IN_USD = 7;
-const KM_IN_ISD = 5;
+/* 
+Нужно проверить может ли он купить новый MacBook за 2000$? 
+Он может брать не только свои деньги, но и взять кредит.
+Ему дадут 500$, только если ему больше 24-х лет и он имеет работу,
+ 100$ если ему просто больше 24-х лет и 0 в ином случае. 
+Напишите функцию, которая принимает данные пользователя и товара и возвращает true или false.
+*/
 
-function calculateW(present){
-  
-   return present * KG_IN_USD;
+const creditMoney500 = 500
+const creditMoney100 = 100
+const PriceMacBook = 2000
+
+
+function canBuyMacBookAndtakeCredit(age, haveWork)
+{  if(age >= 24 && haveWork )
+  {
+     return creditMoney500
+  } 
+  else if( age >= 24 && !haveWork){ 
+    return creditMoney100
+  }
+   else { 
+    return 0 
+  } 
 }
 
-function calculateKm(distance){
-   return distance * KM_IN_ISD;
+function canBuyMac(age, haveWork, money) {
+  return money + canBuyMacBookAndtakeCredit(age, haveWork) >= PriceMacBook;
 }
+console.log(canBuyMac(25, false, 1900)); // Если написать значения  возраст: 25, есть работа или нет: true || false, и денги например 1800, но цена макбука 2000, а у нас только с кредитом например 1900(Короче просто потому что денег у нас будет меньше чем цена мака. вот поэтому фалс,а если бы было бы даже больше например у нас есть 1900 с кредитом будет 2400, короче вот так.), то выведется false, так как у нас денег мне как по функции то есть   return money + canBuyMacBookAndtakeCredit(age, haveWork) >= PriceMacBook, тут понятно что если у нас будет мало денег то будет уже false.  как и с возрастом также.
 
-function getExchangePrice(present1, present2, distance) {
-    const  price1 = calculateW(present1); 
-    const price2 = calculateW(present2);
-    const distancePrice = calculateKm(distance)
-    return price1 + price2 + distancePrice;
-}
 
-console.log(getExchangePrice(1, 2, 10));
+
+
+// function credit(age, haveWork,){
+//   if(age >= 24 && haveWork){
+//  return  `${creditMoney500}`
+//   } else if(age >= 24 && !haveWork){
+//       return  ` ${creditMoney100}`
+//   } else {
+//     return  0 
+//   }
+// } 
+
+//  function canBuyMac(age, haveWork, money) {
+//   return money + credit(age, haveWork) >= PriceMacBook;
+// }
+// console.log(canBuyMac(25, false, 1900));
+ 
