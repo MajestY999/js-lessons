79f785cf919a037577094b6f495e366fd2f8f3e2
MajestY999 <Vilisovartem@gmail.com>
2025-10-28 15:27:07 +1000

    Повторение цикла while

--- app.js
+++ app.js
@@ -1,38 +1,44 @@
-//Цикл while
-for(let i = 1; i < 5; i++){
-    console.log(`Вывод - ${i}`);
-}
-
-
-// let i = 1
-// while (i < 5) {
-//     console.log(`Вывод - ${i}`);
-//     i++;
+// Циклы в циклах 
+// for(let i = 1; i < 5; i++){
+//     console.log(`Цикл 1 - ${i}`);
+//     for(let j = 1; j < 5; j++){
+//         console.log(`Цикл 2 - ${j}`);
+//     }
 // }
 
+// const tasks = [[1, 'Задача 1', ], [2, 'Задача 2']]
 
-// Когда использовать цикл while или for   While: 
-const arr = [1, 4, 8, 7];
+// for(let i = 0;  i< tasks.length; i++ ){
+//     for(let j = 0; j < tasks[i].length; j++) {
+//         console.log(tasks[i][j]);//Тут можно делать отбращение к элементу массива используя скобки массива.
+//     }
+// } 
+
+//Цикл while ()
+// let i = 1   //Обьявляем переменную снаружи цикла while. Внутри даем значение i < 5
+// while(i < 5){
+//     console.log(`Вывод - ${i}`);
+//     i++// Тут внутри мы обьявляем шаг что наша переменная должна увеличиться на один 
+// }
+
+const arr = [1,4, 8, 7]
 
 for(let i = 0; i < arr.length; i++){
-    if(arr[i] > 5){
+    if(arr[i]> 5) {
         break
     }
-console.log(arr[i]); //Выведет 1 и 4
+    console.log(arr[i]); // 1 4
 }
 
-
-let i = 0;
-while(arr[i] <= 5 &&  arr.length){
-console.log(arr[i]);
-i++
+let i =0
+while(arr[i] <= 5 && i < arr.length){
+    console.log(arr[i]);
+    i++//1 4
 }
 
-
-//Также есть другая контсрукция  do{}while
-let j = 0
-
+let j = 0;
 do{
     console.log(j);
-    j++;
-}while(j < 0)
+    j++
+}
+while( j < 0 )//Тут будет код выболняться только 1 раз 
