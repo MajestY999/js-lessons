5407612a8cfc0a671144b92f6b53cf77837a9f50
MajestY999 <Vilisovartem@gmail.com>
2025-11-07 21:31:31 +1000

    reduce

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-//Упражнение - вывод изменений цен
+//reduce
+const operations = [100, -20, 7, -30, 50]
 
-const prices = [[100, 200], [120, 100], [200, 350]]
+let balance = 0;
+for (const operation of operations) {
+    balance += operation
+}
+console.log(balance);// 107
 
-//использовать map filter
-//Нужно преобразовать данные в массив, где будут отображены только положительные изменения цен: [100, 150] и [200, 350]
-// Но массив [120, 100] будет считать отрицательным так как цены упали вниз, а этого не должно произходить. 
+const finalBalance = operations.reduce((acc, operation, i) => {
+    //как оно работает
+    console.log(`Итерация ${i}, acc: ${acc}, value: ${operation} `);
+ return acc += operation// тут теперь мы  быдем выводить чему равен наш acc при возвращении из нашей функции
+}, 0) //через запятую вводим наш первоначальный балансm, и тут мы пишем наш первый начальный баланс и с которого  все как раз иначинается а потом с нулевого то есть первого элемента.
 
-const result = prices.map(
-    price => price[1] - price[0]
-)
-.filter(
-    price => price > 0
-)
+console.log(finalBalance); //107 
 
-console.log(result);
+//0 - acc = 0, value = 100
+//1 - acc = 100, value = -20
+
+const minElement = operations.reduce((acc, operation, i) => {
+    if(operation > acc){
+        return acc
+    }else {
+        return operation
+    }
+   
+}, 0);
+console.log(minElement);//-30, Тут происходит не аккомуляция а возвращение минимально элемента массива
