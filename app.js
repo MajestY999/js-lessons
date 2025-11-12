b0e6ef702c1fe3c39e66f58c5c8d18983508dbd6
MajestY999 <Vilisovartem@gmail.com>
2025-11-12 23:19:56 +1000

    flat() flatMap()

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-// функция some
+//flat() и flatMap()
+//flat
+const prices = [[2,4], [3, 4], [10, /* 20 */ [20, 50]]]
+const res = prices.flat(2)// делает массив плоским то есть уберает из массива внутренние массивы, но если внутри внутреннего массива будет массив то там просто останется массив.
+const  res2 = prices.flatMap(el => el.concat([1])) // [2, 4, 1, 3, 4, 1, 10, Array(2), 1]
+console.log(res);
+console.log(res2);
+//flatMap()
 
-const arr = [2, 4, 4, 10, 20]
- 
-function some(array, element){
-    const res = array.find(el => el === element)// el- это элемент который мы ищем и если элемент el === element равен переданному элементу то у нас будет равен результату если он у мнас есть, если его  у нас нет то будет undefaiend
-    return res === undefined ? false : true;
-}
-
-console.log(some(arr, 2));
-
-console.log(arr.some(el => element === 2));
