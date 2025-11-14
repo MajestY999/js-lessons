e86caaaf2a2d91b2f37e324007f1997b683f6303
MajestY999 <Vilisovartem@gmail.com>
2025-11-14 21:45:53 +1000

    БЫстрое создание массивов

--- app.js
+++ app.js
@@ -1,42 +1,19 @@
-//sort
-const users = ['Вася', 'Маша', 'Катя', 'Аня']
-console.log(users);
-users.sort();//Отсортировал имена по именам от а до я, то есть по алфавиту.
-console.log(users);
+//Быстрое создание массивов.
 
-const operations = [100, -300, -100, 50, 480]
-console.log(operations);
-operations.sort();
-console.log(operations);// отсортировались как строки.   [-100, -300, 100, 480, 50]
+const array = [1,2,3,4,5]
 
+//console.log(new Array(1, 2, 3, 4, 5));//(5) [1, 2, 3, 4, 5]
+console.log(new Array(1, 2, 3, 4, 5));
+const arr2 = new Array(5)// [empty × 5]
+console.log(arr2);//метод map невозможно и нельзя взаимодействовать с пустым массивом.
+console.log(arr2.map(el => el + 1));
+arr2.fill(1, 0, 3)//app.js:12 (5) [1, 1, 1, empty × 2]
+arr2.fill(2,3,5)//(5) [1, 1, 1, 2, 2]
+console.log(arr2);//(5) [1, 1, 1, 1, 1]
 
-// < 0 - a, b - сохраняем порядок.
-// > 0 - b, a - меняем порядок.
-// сортировка на увеличение 
-operations.sort((a, b)=> {
- if(a > b ){
-    return 1;
- }
-if(a < b ){
-    return -1;
-}
-})
-console.log(operations); // [-300, -100, 50, 100, 480]  тут видно что массив работает так что сначала отрицательные значения потом больше нуля и наибольшее а не в разброс.
+const arr3 = Array.from({ length: 5}, (cur, i) => i + 1);//будет [1, 2, 3, 4, 5]
+console.log(arr3);//(5) [5, 5, 5, 5, 5], если только lenght 5  а также () => 5
 
-//Сортировка по убыванию
-operations.sort((a, b)=> {
- if(a < b ){
-    return 1;
- }
-if(a > b ){
-    return -1;
-}
-})
-console.log(operations);//[480, 100, 50, -100, -300] все тоже самое только в разброс.
-
-//Коротка запись той же функции.
-operations.sort((a, b) => a - b)
-console.log(operations);// [-300, -100, 50, 100, 480] все тоже самое как и в вверхней функции а именно с работой на увеличение
-
-
-
+let arr = [1, 2, 10, 15];
+arr.sort()
+console.log(arr);
