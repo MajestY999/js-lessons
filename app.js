895e0bdc3dcf4a5af070ee95a8b2d26e959bcf18
MajestY999 <Vilisovartem@gmail.com>
2025-11-17 14:07:18 +1000

    Упражнение - выделение имени

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-//Повторение с начала темы: Работа со  строками.
-const userName  ='Вася Пупкин'
-console.log(userName[0]);//В
-console.log(userName[1]);//а
-console.log(userName.charAt(2));//с
-//Но эмодзи тратят 2 места в массиве и будет появлятся знак с вопросом так как он не может отображаться в консоле.
-console.log(userName.length);//11
-console.log(userName.indexOf('П'));//5
-console.log(userName.lastIndexOf('а'));
-console.log(userName.includes('Вася'));
-console.log(userName.slice(4));//Остался пробел и фамилия.
-console.log(userName.slice(5, 8));
+const fullUserName = 'Вася aka Terminator Пупкин';
+
+
+const userName = fullUserName.slice(0, fullUserName.indexOf(' '))
+console.log(userName);
+const userSureName = fullUserName.slice(
+    fullUserName.lastIndexOf(' ') + 1, 
+    fullUserName.length
+)
+console.log(userSureName);
