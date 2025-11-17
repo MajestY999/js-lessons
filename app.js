3601c0ecbdd38483a1e7ddbc5a5389158fee0c03
MajestY999 <Vilisovartem@gmail.com>
2025-11-17 14:18:00 +1000

    Преобразование строки

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-const fullUserName = 'Вася aka Terminator Пупкин';
+//Преобразование строки. и методы проверки.
+const str = 'Вася Пупкина'
+console.log(str.includes('а'));
+console.log(str.startsWith('В'));
+console.log(str.endsWith('н'));
+console.log(new String ('Вася Пупкин').includes('а'));//true    
 
+console.log(str.toLowerCase());//полностью маленький текст.
+console.log(str.toUpperCase());//капсом 
+console.log(str.replace('В', 'Ф'));
+console.log(str.replace('П', 'Д'));
+console.log(str.replace('а', 'и'));
+//console.log(str.replaceAll('a', 'и'));
+console.log(str.replace(/a/g, 'и'));
 
-const userName = fullUserName.slice(0, fullUserName.indexOf(' '))
-console.log(userName);
-const userSureName = fullUserName.slice(
-    fullUserName.lastIndexOf(' ') + 1, 
-    fullUserName.length
-)
-console.log(userSureName);
+const str2 = ' Вася Пупкина   \n'
+console.log(str2.trimStart());
