30457b90f5b7a4a9b8433d40502c2f1fe6108097
MajestY999 <Vilisovartem@gmail.com>
2025-11-18 21:37:57 +1000

    Строки и массивы

--- app.js
+++ app.js
@@ -1,54 +1,9 @@
-// Верные номера 
-
-const num1 = '89103235356';
-const num2 = '+79103235356';
-const num3 = '+7(910)3235356';
-const num4 = '+7(910) 3235356';
-const num5 = ' +7(910) 3235356 ';
-
-// не верные номера
-
-const num1Error = '89103235'
-const num2Error = '+7d910d323-53-56'
-const num3Error = '9+7103235356'
-const num4Error = '89103q35356'
-
-// const numberPhone = 8 || 7
-// for( let el = 1;  el < 10; el+= 1 ){
- 
-
-// }
-
-function isPhoneNumber(num){
-    num = num.trim();
-    num = num.replace('+7', '8');
-    if(!num.startsWith('8')){
-        return false
-    }
-    num = num.replaceAll('(', '')
-    num = num.replaceAll(')', '')
-    num = num.replaceAll(' ', '')
-    num = num.replaceAll('-', '')
-    if(num.length != 11){
-        return false
-    }
-    let onlyNumber = true;
-    for(const char of  num){
-        if(isNaN(Number(char))){
-            onlyNumber = false
-            break;
-        }
-    }
-    return onlyNumber    
-}
-
-console.log(isPhoneNumber(num1));
-console.log(isPhoneNumber(num2));
-console.log(isPhoneNumber(num3));
-console.log(isPhoneNumber(num4));
-console.log(isPhoneNumber(num5));
-
-console.log(isPhoneNumber(num1Error));
-console.log(isPhoneNumber(num2Error));
-console.log(isPhoneNumber(num3Error));
-console.log(isPhoneNumber(num4Error));
+//Строки и массивы
+const userFullName = 'Вася Пупкин Васильевич'
+console.log(userFullName.split(' '));
+const [firstName, familyName, lastName] = userFullName.split(' ')
+console.log(firstName);
+console.log(familyName);
+console.log(lastName);
+const arr = ['Ты', 'знаешь', 'JS']
+console.log(arr.join(' '));
