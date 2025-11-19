5a09923ac7a17792ff534494e678f043e043095f
MajestY999 <Vilisovartem@gmail.com>
2025-11-19 15:06:04 +1000

    Обращение к элементам

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
-//Знакомство с обьектами.
-const userArr = ['Вася', 'Пупкин', 24];
-const user = {
-name: 'Вася', 
-surname: 'Пупкин',
-age: 24,
-skills: [
-    'Программирование', 
-    'Готовка'
-],
-1: 1
+//Обращение к элементам.
+const user ={
+        name: 'Вася',
+        surname: 'Пупкин',
+        age: 24,
+        skills: [
+            'Программирование',
+            'Готовка'
+        ],
+        uduBasic: 'Школа 10',
+        eduPro: 'МФТИ'
 };
+
+console.log(user);
+console.log(user.skills[0]);//Можно использовать порядок операторов.
+console.log(user['skills']); //(2) ['Программирование', 'Готовка']
+const level = 'Pro'
+console.log(user['edu' + level]); //МФТИ
+
+// const res = prompt('Введите свойство');
+// console.log(user[res]);
+user.cuty = 'Москва'
+user['city'] = 'Москва'
+console.log(user);
+
+user.age = 30
+user['age'] = 30
+console.log(user);
