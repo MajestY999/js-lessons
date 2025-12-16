79d68f4cd00620b64b089f48d71067ec61f077f1
MajestY999 <Vilisovartem@gmail.com>
2025-12-16 16:43:15 +1000

    Пример работы call stack

--- app.js
+++ app.js
@@ -1,56 +1,16 @@
-//Контекстт исполнения и stack вызова
+//Пример работы call stack
+const sum = 1;
+console.log('start');
+console.log(sum);
 
-//Контекст исполнения 
-//Копиляция =>  создание глобального контекста -> Выполнение кода верхнего уровня <<---(К нему относится код ) -> Выполнение функции и ожидание callback  
-
-const user = {
-    name: 'Вася',
-    roles: ['admin']
+function add5(n){
+  n = n + 5; 
+  if(n > 100){
+    return n ;
+  }
+  console.log(n);
+  return add5(n)
 }
 
-function isUserAdmin(user){
-    return user.roles.includes('admin')
-}
-
-console.log(isUserAdmin(user));//true  
-
-const createUserWithAge = (user, age) => {
-    const newUser = {
-        ...user,
-         age
-    }
-    return newUser
-}
-
-//Global
-//user
-//function
-//createUserWithAge
-
-//isUserAdmin()
-//-
-
-//IsuserAdmin()
-// newUSer
-
-
-
-//Что внутри контекста 
-//Находится 3 важных= элемента
-
-//Окружение переменных
-//let const var
-//function
-//arguments
-
-
-//Scope Chains --- ссылки на переменные вне текущего контекста
-
-//this (Клучевое слово this, и оно сслыкается на наш основной контекст)
-
-//Stack  Это  Некоторая последовательность при которой вызывается наш код. 
-
-// у нас есть переменные которые умножсами на себя
-//Есть функция в которой есть вызво двойного элем.
-
-// а вот const a = 5
+add5(sum)
+console.log('ended');
