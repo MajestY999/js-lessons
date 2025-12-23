a5196b6943e32753ff7811ab9fe394ba74e3ff76
MajestY999 <Vilisovartem@gmail.com>
2025-12-23 16:16:51 +1000

    Примеры scope chain

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
+//Примеры scope chain или контекстами 
 'use strict'
-
-let myCoolVarible = 1;
-
-
-if(true){
-myCoolVarible = 3
-} 
-console.log(myCoolVarible);
-
-function a(b, b){//должны быть разные переменные 
-console.log(b);
+let successMessage = 'Успех'
+const user = {
+  name: 'Вася',
+  roles: []
 }
-a(1, 1)
+
+function addRole(user, role){
+  if(role == 'admin'){
+    const message = 'Ошибка'
+    console.log(message);
+    return user
+  }
+  user.roles.push(role);
+  let successMessage = 'Ура';
+  console.log(successMessage);//successMessage машина искала его по всему коду даже если его нет внутри функции
+
+  function logRoles(){
+    console.log(user.roles);
+  }
+  logRoles()//тут потому что есть внутри обьявленна в функции user
+  return user;
+}
+console.log(addRole(user, 'admsin'));
+console.log(successMessage);//но если тут прописать тогда просто будет работать текущее сообщение как выше то есть если оно есть внутри то оно будет только с внутренним сообщением
