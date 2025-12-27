d3868564b2483b97b0a0fa44b20f06681f68c7eb
MajestY999 <Vilisovartem@gmail.com>
2025-12-27 20:42:52 +1000

    Поднятие и Пример поднятия.

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
-//Примеры scope chain или контекстами 
-'use strict'
-let successMessage = 'Успех'
-const user = {
-  name: 'Вася',
-  roles: []
+//Поднятие 
+//Это возможность использовать определенные типы переменных до того, как они были обьявлены.
+
+//Цикл переменных
+//обьявление: это var a(поднятие)
+//присвоение: a = 5
+//использование: a + 10
+
+//Что такое TDZ
+// function addRole(){
+//   user.roles.push(role); //это tdz то етсь где наша role не доступна Temporal Dead Zone
+//   let successMessage = 'Ура' // это тоже tdz Temporal Dead Zone
+//   const role = 'test'
+// }
+
+//Пример поднятия
+// console.log(a);ошибка и также будет с изменяемой переменой let 
+// console.log(b ); ошибка точнее будет undefined
+const  a = 3;
+var b = 2;
+console.log(b);//но тут все в порядке
+
+function addUser(){
+  //console.log('user added');
+
+
 }
 
-function addRole(user, role){
-  if(role == 'admin'){
-    const message = 'Ошибка'
-    console.log(message);
-    return user
-  }
-  user.roles.push(role);
-  let successMessage = 'Ура';
-  console.log(successMessage);//successMessage машина искала его по всему коду даже если его нет внутри функции
+addUser()
 
-  function logRoles(){
-    console.log(user.roles);
-  }
-  logRoles()//тут потому что есть внутри обьявленна в функции user
-  return user;
+const arr1 = ()=> {
+  console.log('arr1');
 }
-console.log(addRole(user, 'admsin'));
-console.log(successMessage);//но если тут прописать тогда просто будет работать текущее сообщение как выше то есть если оно есть внутри то оно будет только с внутренним сообщением
+arr1()
+
