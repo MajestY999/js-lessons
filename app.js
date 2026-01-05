f8b7b3c40103bf678dbba951a30a64863bcdba34
MajestY999 <Vilisovartem@gmail.com>
2026-01-05 17:53:42 +1000

    Поиск элемента

--- app.js
+++ app.js
@@ -1,59 +1,20 @@
-//Повторение массивов
-const role1 = 'admin'
-const role2 = 'user'
-const role3 = 'superuser'
+//Поиск элемента масисва
+const roles = ['user', 'admin', 'manager']
 
-const roles = ['admin', 'user', 'superuser'];
-const userInfo = ['Аня', 25]
-console.log(roles);
-console.log(roles[0]);
-console.log(roles.length);
-//Что бы получить последний элемент массива: 
-console.log(roles[roles.length - 1]);
+console.log(roles.indexOf('admin'));//если элемент есть в массиве то будет 1 даже если их два в массиве.
+const elIndex = roles.indexOf('superuser');//Данный элемент отстутствует получается будет -1, точно также как с true/false
+console.log(elIndex);
 
-console.log(roles.at(0));//Получим admin
-//Также мы можем также просто найти последний элемент массива
-console.log(roles.at(-1));
-
-const usersAge = [2040 - 2022, 10 > 0 ? 5 : 0 ] 
-console.log(usersAge);
-
-
-const userNames = new Array('Вася', "Петя", "Катя")
-console.log(userNames);
-
-function square(el){
-    return el * el
+if(roles.indexOf('admin')>= 0 ){
+ console.log('Доступ есть');
+}else{
+    console.log('Нет доступа');
 }
 
-console.log(square(6));
-
-const arr = [2, 4, 8, 16]
-
-const squareNums =  arr
-.map((nums)=>{
-return nums * 2
-})
-
-console.log(squareNums);
-
-const users = ['Аня', 'Вика', 'Катя'];
-console.log(users);
-users[2] = 'Кристина'
-console.log(users);
-console.log(users.length);
-
-//добавление массива
-users.push('Никита')//push возращает длинну массива.
-console.log(users);
-users.unshift('Вася')//Добавили в самое начало имя Вася
-console.log(users);
-
-const el = users.pop()//Удаляет последний элемент массива
-console.log(el);
-console.log(users);
-
-const el2 =users.shift()
-console.log(el2);//Вася был удален 
-console.log(users);
-
+console.log(roles.includes('admin'));//true
+console.log(roles.includes('superuser'));//false
+if(roles.includes('admin') ){//С этим вариантом удобнее
+ console.log('Доступ есть');
+}else{
+    console.log('Нет доступа');
+}
