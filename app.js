4dbd036d12611b41b09c2ee96b3e89f188ebd3e9
MajestY999 <Vilisovartem@gmail.com>
2026-01-06 18:40:46 +1000

    Повторение Rest операторов

--- app.js
+++ app.js
@@ -1,31 +1,5 @@
-//Упражнение Обновление списка задач 
-const tasks = ['Задача 1']
- 
-
-function Add(task){
-    tasks.push(task)//Встает в конце 
-}
-
-function remove(task){
-    const index = tasks.indexOf(task)// поиск элемента где он лежит.
-    if(index !== -1){
-        tasks.splice(index, 1)//выкидываемя элемент из массива "Если мне нужно выкинуть лишний кирпич из стены, мне нужно знать его номер (индекс) и сколько штук выкинуть (1)".
-    }
-}
-
-function prioritaze(task){
-    const index = tasks.indexOf(task)
-    if(index !== -1){
-        const item = tasks.splice(index, 1)[0]
-        tasks.unshift(item)// добавляется в начало
-    }
-}
-
-Add('Работать')
-Add('Учиться')
-Add('Пылесосить')
-console.log(tasks);
-remove('Учиться')
-console.log(tasks);
-prioritaze('Пылесосить')
-console.log(tasks);
+//Rest операторы
+const data = [1,2, 3, 4, 5, 6]
+const [one, two, ...other] = data
+console.log(one, two, ...other);// ... это рест оператор, так же есть спред оператор. рест это три точки то есть являются остальными данными чтобы не переписывать массив полностью 
+//Но если вызвать массив const [...others ] то просто выведем тот же массив
