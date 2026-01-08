03b756221f5ca8f5149f37c8c8107881519559d0
MajestY <majesty@MacBook-Air-MajestY.local>
2026-01-09 00:01:12 +1000

    Повторение Цикла While

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-const url = '<https://purpleschool.ru/course/javascript>'
+// Цикл While
+// let i = 1
+// while(i < 5){
+// console.log(`Вывод - ${i}`);
+//  i++
 
+// }
 
-function disUrl(){
-    const [protocol, domain, ...path] = url.split('/')
-    return `${protocol}/${domain}/${path.join('/')}`
+const arr = [1, 4, 8, 7]
+
+for(let i = 0; i < arr.length; i++){
+    if(arr[i] > 5){
+        break;
+    }
+    console.log(arr[i])
 }
-console.log(disUrl());
+let i = 0;
+while(arr[i] <= 5 && i < arr.length){
+    console.log(arr[i])
+    i++;
+}
+
+let j = 0
+do{
+    console.log(j)
+    j++
+}while(j < 0 )
