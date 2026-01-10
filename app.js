f9ecfb21d663702e11637e4105838d24351cd5c0
MajestY999 <Vilisovartem@gmail.com>
2026-01-10 22:43:12 +1000

    Упражнение кошелек

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-//Методы обьектов
-const user = {
-    name: 'Вася',
-    surname: 'Пупкин',
-    age: 24,
-    getFullName: function(){
-        console.log(this);
-        return this.name + ' ' + this.surname
+//Упражнение кошелек
+const wallet = {
+    balance: 0,
+    operations: [],
+    increase: function(sum, reason){
+        this.balance += sum
+        this.operations.push(`${reason}: true  `)
+        return `Поплнен баланс на ${sum}, Причина: ${reason}`
+    },
+    decrease: function(sum, reason){
+        if(this.balance < sum){
+            return `Отрицательный баланс или недостаточно денег на счете денег на счете`
+        }
+        this.balance -= sum
+        this.operations.push(`${reason}: true`)
+        return `Сумма снятия: ${sum} По причине: ${reason}`
+    }  , 
+    getOperationLenght: function(){
+        return this.operations.length
     }
-};
-console.log(user.getFullName());
-const arr = [1, 2]
-arr.sort();
 
+}
 
+console.log(wallet.increase(100,'Пополнение с помощью банкомата'));
+
+console.log(wallet.increase(100, 'Перевод'));
+console.log(wallet.decrease(150, 'Повторное снятие денег'));
+console.log(wallet.getOperationLenght());
