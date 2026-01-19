a8f13fe45a92e3c1be2c0e20c94cf27dab6bf269
MajestY999 <Vilisovartem@gmail.com>
2026-01-19 20:35:03 +1000

    Контекст в методах

--- app.js
+++ app.js
@@ -1,45 +1,30 @@
-//Ключевое слово this
+//Контекст в методах
+'user strict'
 
-//this это переменная которая создается внутри контекста исполнения. Она указывает на владельца этой функции.
-//this не является статичным и опеределяется только при вызове функции в зависимости от того, как она названа 
-// в стрелочной функции он работает this родительского scope то есть не во внешний а только внутри в функции он является undefined
-//в методе работает на объекте этого метода
-
-//Пример использования this 
-//'use strict'
-
-//console.log(this);//он равен тому ж еокну потому что получает те же данные и чем является приложение
-
-function addNum(num1, num2){
-    //console.log(this);//this = undefiend, это он выводит когда ипользуем strict mode 
-    return num1 + num2
-}
-
-addNum()
-
-const addNum2 = (num1, num2)=> {
-    console.log(this);
-    return num1 + num2
-}
-
-//addNum2()
-//Как работает  this с объектами 
 
 const user = {
-    name: 'Вася',
-    surname: 'Пупкин',
-    getFullName: function(){
-        console.log(this);
-        return this.name + ' ' + this.surname
+    firstName: 'Вася',
+    lastName: 'Пупкин',
+    age: 20,
+    getUserInfo: function(){
+        console.log(this);//тут будет все верно в виде нашего объекта
+        console.log(`${this.firstName}  ${this.lastName}`);
+        const canDrink=()=> {
+            if(this.age >= 18){
+                console.log('Может пить');
+            }else{
+                console.log('Не может пить');
+            }
+           
+        }
+         canDrink()
+
+    },
+    getUserInfoArrow: () => {
+        console.log(this);//а тут он у нас не работает
+        console.log(`${this.firstName}  ${this.lastName}`);//у стрелочных функций this отсутствует
     }
 }
-user.getFullName()
-//console.log(user.getFullName());
-const user2 = {
-    name: 'Марина',
-    surname: 'Катц'
-}
 
-user2.getFullName = user.getFullName;
-user2.getFullName()
-
+user.getUserInfo()
+//user.getUserInfoArrow()
