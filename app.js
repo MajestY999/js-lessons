4695bcadeebad4a01ee4cd565af881a53e13897a
MajestY999 <Vilisovartem@gmail.com>
2026-01-19 20:44:09 +1000

    arguments

--- app.js
+++ app.js
@@ -1,30 +1,17 @@
-//Контекст в методах
-'user strict'
+//Argument
+'use strict'
 
-
-const user = {
-    firstName: 'Вася',
-    lastName: 'Пупкин',
-    age: 20,
-    getUserInfo: function(){
-        console.log(this);//тут будет все верно в виде нашего объекта
-        console.log(`${this.firstName}  ${this.lastName}`);
-        const canDrink=()=> {
-            if(this.age >= 18){
-                console.log('Может пить');
-            }else{
-                console.log('Не может пить');
-            }
-           
-        }
-         canDrink()
-
-    },
-    getUserInfoArrow: () => {
-        console.log(this);//а тут он у нас не работает
-        console.log(`${this.firstName}  ${this.lastName}`);//у стрелочных функций this отсутствует
-    }
+function sumNum(num1, num2){
+    console.log(this);
+    console.log(arguments);
+    return num1 + num2
 }
 
-user.getUserInfo()
-//user.getUserInfoArrow()
+console.log(sumNum(1, 4));
+
+
+const sumNumArr = (num1, num2)=> {
+ return num1 + num2
+}
+
+console.log(sumNumArr(1, 4));
