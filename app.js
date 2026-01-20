b257f55f3486345148b790510475e9759b5dfd73
MajestY999 <Vilisovartem@gmail.com>
2026-01-20 21:04:27 +1000

    bind

--- app.js
+++ app.js
@@ -1,33 +1,25 @@
-//Call, apply. как мы можем управлять нашим this
+//bind
 'use strict'
 
 const audi = {
     make: 'Audi',
     model: 'A3',
-    year: 2021,
-    damages: [],
-    addDamage(part, rate){
-        console.log(`У авто${this.make} ${this.model} ${this.year} добавлено: повреждение ${part} со степенью ${rate}`);
-        this.damages.push({
-            part,
-            rate
-        })
-    }
-};
-// audi.addDamage('Капот', 1)
-
-const bmw = {
-    make: 'BMW',
-    model: 'X5',
-    year: 2022,
     damages: []
+};
+
+const carManipulation = {
+    addDamage(rate, part){
+        this.damages.push({rate,part})
+        console.log(`Добавили повреждения на ${this.make} ${this.model}`);
+        
+    }
 }
 
-// bmw.addDamage = audi.addDamage;
-// bmw.addDamage('Бампер', 2);
-const addDamageFunc = audi.addDamage; //Получим много ошибок так как это не объект а просто переменная которая берет какую либо функцию 
-addDamageFunc.call(bmw, 'Бампер', 2)//То есть мы хотим вызваь нашу фнукцию с нашими переменными бампер и 2
-addDamageFunc.call(audi, 'Капот', 1)
+const addDamageAudi = carManipulation.addDamage.bind(audi);//первым идет контекст а дальше что то еще
+addDamageAudi('Крыло', 3);
+console.log(audi);
 
-addDamageFunc.apply(bmw, ['Бампер', 2])//Тут у нас вызывается так что нам нужно применить методна каком либо объекте и нужно указать массив аргумента с нашими данными 
-//Разница в том что они будут вызываться по разному один через запятую(но и можно в первом через ...spred оператор) второй через массив.
+const addDamageAudiRoof = carManipulation.addDamage.bind(audi, 'Крыша');//первым идет контекст а дальше что то еще
+addDamageAudiRoof(5)
+addDamageAudiRoof(3)
+console.log(audi);
