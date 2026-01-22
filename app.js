ae39bda367111e6edb3e465e75592a01ba41d554
MajestY999 <Vilisovartem@gmail.com>
2026-01-22 13:53:00 +1000

    Повторение bind

--- app.js
+++ app.js
@@ -1,38 +1,22 @@
-//Повторение call, apply
+//bind
 'use strict'
 
 const audi = {
     make: 'Audi',
-    model: 'A3',
-    year: 2021,
-    damages: [],
-    addDamage(part, rate){
-        console.log(`У авто ${this.make} ${this.model} ${this.year} добавлено повреждение - повреждение ${part} по степенью ${rate}`);
-        this.damages.push({
-            part, 
-            rate
-        })
-        
-    }
-}
-
-// audi.addDamage('Капот', 1)
-
-const bmw = {
-    make: 'BMW',
-    model: 'X5',
-    year: 2022,
+    model: 'A3', 
     damages: []
 }
 
-// bmw.addDamage = audi.addDamage;
-// bmw.addDamage('Бампер', 2);
+const carManipulations = {
+    addDamage(part, rate){
+        this.damages.push({part, rate})
+        console.log(`Добавление повреждений на ${this.make} ${this.model}`);
+    }
+}
 
-const addDamageFunc = audi.addDamage
-console.log('Call:');
-addDamageFunc.call(bmw,'Бампер', 2)   //он вызывает нашу функцию и говорит на каком контексте и с какми аругментами
-addDamageFunc.call(audi, 'Капот', 2)
-console.log('Apply:');
-addDamageFunc.apply(bmw, ['Бампер', 2])//Нам нужно пременить метод на каком лмбо объекте и вызвать массив аргумента 
-addDamageFunc.apply(audi, ['Бампер', 2])
-//Но вызвать window тут нельзя
+const addDamageAudi = carManipulations.addDamage.bind(audi);
+addDamageAudi('Крыло', 3)
+
+const addDamageAudiRoof = carManipulations.addDamage.bind(audi, 'Крыша');
+addDamageAudiRoof(5);
+console.log(audi);
