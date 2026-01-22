9d27e46465467004946e97dbcf83302fd6950eca
MajestY999 <Vilisovartem@gmail.com>
2026-01-22 21:42:02 +1000

    Замыкания и упражнение - упраление this

--- app.js
+++ app.js
@@ -1,23 +1,53 @@
 //Замыкание
 'use strict'
 
-function changeBalance(){
-  let balance = 0;
-  return function(sum){
-    balance += sum;
+// function changeBalance(){
+//   let balance = 0;
+//   return function(sum){
+//     balance += sum;
     
-    console.log(`Баланс: ${balance}`);
+//     console.log(`Баланс: ${balance}`);
+//   }
+// }
+// //Замыканеи это комбинация функций и лексического окружения, в котором эта функция была определена. 
+// //Простыми словами: функция помнит, в каком контексте она была создана и может его использовать
+
+// const change = changeBalance();//Является не разрывной частью нашей новой функцией и замыкание имеет более сильный приоритет 
+// change(100)
+// change(-50)
+// change(200)
+// console.dir(change);
+
+// const change2 = changeBalance();//Функция начнется с нуля и увеличится на сто как по нашей функции
+// change2(100);
+// console.dir(change2);
+
+
+const userInfo = {
+  balance: 0,
+  operations: 0, 
+  increase(sum){
+    this.balance += sum, this.operations++;
   }
 }
-//Замыканеи это комбинация функций и лексического окружения, в котором эта функция была определена. 
-//Простыми словами: функция помнит, в каком контексте она была создана и может его использовать
 
-const change = changeBalance();//Является не разрывной частью нашей новой функцией и замыкание имеет более сильный приоритет 
-change(100)
-change(-50)
-change(200)
-console.dir(change);
+function user(){
+  const userObj ={...userInfo}
+  return function(){
+    return userObj
+  }
+}
 
-const change2 = changeBalance();//Функция начнется с нуля и увеличится на сто как по нашей функции
-change2(100);
-console.dir(change2);
+const user1 = user()
+
+user1().increase(100)
+user1().increase(100)
+user1().increase(-50)
+
+console.log(user1());
+
+const user2 = user();
+user2().increase(100);
+user2().increase(200)
+user2().increase(-100)
+console.log(user2());
