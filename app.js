3ba8eb51237a7a7561d29dc65b541674ef87168e
MajestY999 <Vilisovartem@gmail.com>
2026-01-31 21:22:31 +1000

    Создание html на лету

--- app.js
+++ app.js
@@ -1,38 +1,29 @@
-// function submitForm(){
-// const input = document.querySelector('.input').value
-// if(!input){
-//     return;
+//добавление html на лету
+'use strict'
+function submitForm(){
+const input = document.querySelector('.input').value
+if(!input){
+    return;
+}
+document.querySelector('.panel').innerText = input;
+document.querySelector('.input').value = ' ';
+// document.querySelector('.notification').classList.add('notification_active')
+document.querySelector('.notification').classList.remove('notification_hidden')
 
+//document.querySelector('.notification').classList.replace('button', 'newButton')
+}
 
+function inputChanged(e){
+if(e.code == 'Enter'){
+    submitForm()
+}
+}
+//создание html на лету
+const panelText = 'Панель'
+const newElement = document.createElement('div');
+newElement.setAttribute('user-id', 1);
+newElement.classList.add('panel');
+newElement.innerText = 'Button';
+newElement.innerHTML = `<button class=${panelText}">${panelText}</button>`
+document.querySelector('.test').appendChild(newElement);
 
-// }
-// document.querySelector('.panel').innerText = input;
-// document.querySelector('.input').value = ' ';
-// // document.querySelector('.notification').classList.add('notification_active')
-// document.querySelector('.notification').classList.remove('notification_hidden')
-
-// //document.querySelector('.notification').classList.replace('button', 'newButton')
-// }
-
-// function inputChanged(e){
-// if(e.code == 'Enter'){
-//     submitForm()
-// }
-// }
-
-// // document.querySelector('.button').addEventListener(SubmitForm, function(e){ 
-// //     console.log(e);
-// // }) //обработчик событий 
-console.log(document.querySelectorAll('.one')[0].innerHTML)
-
-console.log(document.querySelectorAll('.one')[1].innerHTML)
-
-console.log(document.querySelector('#two').innerText)//id = two или так
-
-console.log(document.querySelector('[user-id]').innerText)
-
-
-//console.log(document.getElementById('two').innerHTML);
-console.log(document.getElementsByClassName('one'));
-console.log(document.querySelector('one'));
-console.log(document.querySelector('[user-id=4]'));
